c4def9e6d85ee0c6d83ec107d87ac9989ae472db
Prabal <prabalparihar95@gmail.com>
2023-02-13 13:59:12 +0530

    Deployed Address(Backend set)

---
+++ const/addresses.ts
@@ -0,0 +1,6 @@
+export const TRANSFER_CONTRACT_ADDRESS = '0x97Fd41b3d6f343ef0C3Fb9dAf342c3f47E2B48A7';
+export const CALIM_TOKEN_CONTRACT_ADDRESS = '0xAC9cFB95c05f6755Daa867F5732D7a7Af7521585';
+
+export const HERO_IMAGE_URL = 'ipfs://QmUWVRuyFuVfo45estsJt2VaQUSEc4uwcPoeWFaeYxERua/seanwatase_isometric_crypto_transferring_from_one_wallet_to_ano_96084ff1-fe03-4dfd-8783-a3ecc85a24da.png';
+export const FEATURES_IMAGE_URL = 'ipfs://QmcTgfb2AXj1TcVj4i9jSgZc8A8LhxHYCXbu5Mngsx7iVT/Coins.png';
+export const CLAIM_TOKEN_IMAGE = 'ipfs://QmUYpRzQE6bebNuuVriyd2n163wAKu1pNRusqQraeRU5bX/seanwatase_isometric_black_crypto_coin_transparent_background_3dcbfe44-babc-45f5-9d43-881d85d2293d.png';
